8b1efa719d29d887583aecb17dff820f35ef1f10
Mukhtar Otarbayev <mukhtar.otarbayev@gmail.com>
2020-10-18 17:22:00 +0900

    Updates

---
+++ precache-manifest.f4356f09a394d5a4fc49d9dcaf91f03f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1ae8f6b58dfaf895be2a0bd4f35a05",
+    "url": "/robots_app/index.html"
+  },
+  {
+    "revision": "ceca06adeacd5ff0793a",
+    "url": "/robots_app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a5c7830b19facea12b50",
+    "url": "/robots_app/static/css/main.3f7cc87b.chunk.css"
+  },
+  {
+    "revision": "ceca06adeacd5ff0793a",
+    "url": "/robots_app/static/js/2.068396ae.chunk.js"
+  },
+  {
+    "revision": "cb4738a14a2ef86f47cb194746bb3116",
+    "url": "/robots_app/static/js/2.068396ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c7830b19facea12b50",
+    "url": "/robots_app/static/js/main.0e409bf0.chunk.js"
+  },
+  {
+    "revision": "e1303fe8b4edcad7f0a2",
+    "url": "/robots_app/static/js/runtime-main.d807b7bf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots_app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
